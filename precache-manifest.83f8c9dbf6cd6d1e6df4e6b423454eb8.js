a5f818969f7d4c4397a3d653dbe74ff6e51d7e53
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-23 10:51:12 +0000

    Deploy in-in/hydrogen to github.com/in-in/hydrogen.git:gh-pages

---
+++ precache-manifest.83f8c9dbf6cd6d1e6df4e6b423454eb8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d24c903a5be76ef59383b625075a8b1",
+    "url": "/hydrogen/static/media/press-start-2p-v7-latin-ext_latin-regular.0d24c903.woff2"
+  },
+  {
+    "revision": "074e10dfa8a680d067e776afed1cf8a3",
+    "url": "/hydrogen/static/media/press-start-2p-v7-latin-ext_latin-regular.074e10df.woff"
+  },
+  {
+    "revision": "bcd44d42d795e26c8cb3",
+    "url": "/hydrogen/static/js/runtime~main.bcd44d42.js"
+  },
+  {
+    "revision": "4d08abbc899e95724722",
+    "url": "/hydrogen/static/js/main.4d08abbc.chunk.js"
+  },
+  {
+    "revision": "75dd14e5085a44a636b5",
+    "url": "/hydrogen/static/js/2.75dd14e5.chunk.js"
+  },
+  {
+    "revision": "4d08abbc899e95724722",
+    "url": "/hydrogen/static/css/main.f82bf6f6.chunk.css"
+  },
+  {
+    "revision": "75dd14e5085a44a636b5",
+    "url": "/hydrogen/static/css/2.21cf2022.chunk.css"
+  },
+  {
+    "revision": "b67e10d6c1a011612a47c21c11713f53",
+    "url": "/hydrogen/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/hydrogen/precache-manifest.83f8c9dbf6cd6d1e6df4e6b423454eb8.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/hydrogen/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
